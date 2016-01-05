4a2a388a4fc3683091c4c2f9443c8ef59960d9c1
Joffray Billon <joffray.billon@gmail.com>
2016-01-05 15:23:11 +0100

    Change data xml to json
    I juste change XML data storage to JSON for optimisation reason

--- PPE/textxml.php
+++ PPE/textxml.php
@@ -1,21 +1,23 @@
+<meta charset="UTF-8">
 <form method="POST" action="">
-<textarea name="titre"></textarea>
-<textarea name="content"></textarea>
+<textarea name="titre" placeholder="Intitulé du menu"></textarea>
+<textarea name="content1"></textarea>
+<textarea name="content2"></textarea>
 <input value="Send" type="submit">
 </form>
 <?php
 if(!empty($_POST))
 {
 	var_dump($_POST);
-	$tab=array($_POST["titre"]=>$_POST["content"]);
+	$tab=array($_POST["titre"]=>array('content1'=>$_POST["content1"], 'content2'=>$_POST["content2"]));
 	var_dump($tab);
-	$xml=xmlrpc_encode($tab);
-	var_dump($xml);
-	$axml=xmlrpc_decode($xml);
-	var_dump($axml);
+	$json=json_encode($tab);
+	var_dump($json);
+	$ajson=json_decode($json);
+	var_dump($ajson);
 
-	$monXML=fopen("data/data.xml", "w+");
-	fwrite($monXML, $xml);
-	fclose($monXML);
+	$monJSON=fopen("data/data.json", "w+");
+	fwrite($monJSON, $json);
+	fclose($monJSON);
 }
 ?>
